540ea50f2c6c379b819825b6c735d1bfe5cfb5bc
yasirjr1 <malik.j.ir1.212@gmail.com>
2025-04-18 06:54:07 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Y28j0awL#NAWK6UT9DHcyCPXJqmHScJ-GblA1tbaR0RcNQl7l2qE",
+SESSION_ID: process.env.SESSION_ID || "levanter_182fef7d4ac0c4457cbdcb77fe650670ba",
 MONGODB: process.env.MONGODB || "mongodb+srv://Maher:Zubair@sigma-male.ggwx4gc.mongodb.net/?retryWrites=true&w=majority",
-  MODE :  process.env.MODE || "", // PUT PUBLIC OR PRIVATE
+  MODE :  process.env.MODE || "public", // PUT PUBLIC OR PRIVATE
 };
